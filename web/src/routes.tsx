dfdcd34670e6e48b9c0a47102f7a45a7c58f1725
LucasMathW <lucasag0408@outlook.com>
2020-10-19 18:59:34 -0400

    Adicionando código fonte da aplicação mobile

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,14 +3,18 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 //COMPONENTES
 import Landding from './pages/Landding';
-import Orphanages from './pages/OrphanagesMap';
+import OrphanagesMap from './pages/OrphanagesMap';
+import CreateOrphanage from './pages/CreateOrphanage';
+import Orphanage from './pages/Orphanage'
 
 function Routes(){
   return (
        <BrowserRouter>
           <Switch>
             <Route path="/" exact component={Landding} />
-            <Route path="/app" component={Orphanages} />
+            <Route path="/app" component={OrphanagesMap} />
+            <Route path="/orphanage/create" component={CreateOrphanage} />
+            <Route path="/orphanage/:id" component={Orphanage} />
           </Switch>  
        </BrowserRouter> 
   );
