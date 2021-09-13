ec17b5dff87df46a91bdbfd754a49f9c181ef4ce
LucasMathW <lucasag0408@outlook.com>
2021-09-13 14:28:26 -0400

    add haibow

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import 'express-async-errors';
 import path from 'path';
 import cors from 'cors';
+import hl from 'handy-log';
+
 
 import './database/connection';
 import errorHandler from './errors/handle';
@@ -15,4 +17,6 @@ app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
 
-app.listen(3333);
+app.listen(3333, ()=>{
+  hl.rainbow(`server is running on port: ${3333}`)
+});
